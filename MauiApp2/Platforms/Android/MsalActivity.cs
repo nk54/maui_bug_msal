2596c7f524a071caba7b3449fc238e143a7d187b
Nicolas Krier <nkrier@atawiz.fr>
2022-11-03 10:06:39 +0100

    first commit

---
+++ MauiApp2/Platforms/Android/MsalActivity.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Content;
+using Microsoft.Identity.Client;
+
+//namespace MauiApp2;
+
+//[Activity(Exported = true)]
+//[IntentFilter(new[] { Intent.ActionView },
+//    Categories = new[] { Intent.CategoryBrowsable, Intent.CategoryDefault },
+//    DataHost = "auth",
+//    DataScheme = "msalf84afb14-950d-4745-be95-a82c2f9b4a26")]
+//public class MsalActivity : BrowserTabActivity
+//{
+//}
